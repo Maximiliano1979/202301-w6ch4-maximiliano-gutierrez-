82ed5c1e04026b636f962604b6ba27c656054231
Maximiliano Gutierrez <hmgutierrezfernandez@gmail.com>
2023-02-16 20:26:30 +0100

    Upload server

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,16 @@
-let a = 3;
-a = 2;
+import "./loadEnvironment.js";
+import express from "express";
+import createDebug from "debug";
+
+const debug = createDebug("things:root");
+
+const app = express();
+const port = process.env.PORT ?? 3000;
+
+app.listen(port, () => {
+  debug(`Working in port ${port}`);
+});
+
+app.get("/things", (req, res) => {
+  res.status(200).json({ react: 3 });
+});
